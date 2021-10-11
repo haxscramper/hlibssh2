ac246b3b0465b6f6eea9d84bbbb036a2eed78dbd
haxscramper <haxscramper@gmail.com>
2021-10-11 20:50:40 +0300

    [WIP] Add hcparse library setup

---
+++ src/hlibssh2/hcparse_generate.nim
@@ -0,0 +1,18 @@
+import hcparse
+
+let
+  dir     = AbsDir"/usr/include"
+  tmpDir  = getAppTempDir() / "libssh2"
+  files   = @[
+    dir /. "libssh2.h",
+    dir /. "libssh2_sftp.h",
+    dir /. "libssh2_publickey.h"
+  ]
+  map     = expandViaWave(files, tmpDir, baseCParseConf)
+  conf    = initCSharedLibFixConf("ssh2", false, dir, map)
+  wrapped = tmpDir.wrapViaTs(conf)
+  outDir  = currentAbsSourceDir()
+  grouped = writeFiles(outDir, wrapped, cCodegenConf)
+
+validateGenerated(grouped)
+echo "done"
